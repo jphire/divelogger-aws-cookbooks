57d82b1fe07a5cfb8d25f3348660db8e362d03a9
jphire <janne.laukkanen83@gmail.com>
2016-06-04 13:43:52 +0300

    fix/ pin chef version to 11.10.4 to fix mongo cookbook

--- divelogger-mongodb/metadata.rb
+++ divelogger-mongodb/metadata.rb
@@ -6,6 +6,8 @@ description      'Installs/Configures divelogger-mongodb'
 long_description 'Installs/Configures divelogger-mongodb'
 version          '0.1.0'
 
+chef_version '11.10.4'
+
 depends 'build-essential'
 depends 'git'
 depends 'mongodb3'
